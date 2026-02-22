fcbc42be5107a9b3bc5e2d57ff6831207a600540
hugo <hugo@mlhwrfrench.co.uk>
2026-02-22 01:14:23 +0000

    feat: added basic memory security utils

--- src/utils/memory.h
+++ src/utils/memory.h
@@ -1,4 +1,19 @@
 #ifndef SPM_MEMORY_H
 #define SPM_MEMORY_H
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#if defined(PLATFORM_MACOS) || defined(PLATFORM_LINUX)
+#include <sys/mman.h>
+#endif
+
+void secure_free(void *ptr, size_t len);
+
+void *secure_alloc(size_t len);
+
+void *secure_lock(void *ptr, size_t len);
+
+void secure_unlock(void *ptr, size_t len);
+
 #endif // SPM_MEMORY_H
