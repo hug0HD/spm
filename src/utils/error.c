951a3f00fbf9f7e1d894b997f394dfc2442f1ff1
hugo <hugo@mlhwrfrench.co.uk>
2026-02-22 00:10:13 +0000

    feat: Added error codes and basic loggin macros

--- src/utils/error.c
+++ src/utils/error.c
@@ -1,1 +1,26 @@
 #include "error.h"
+
+const char *spm_strerror(SpmError err) {
+  switch (err) {
+  case SPM_OK:
+    return "OK";
+  case SPM_ERR:
+    return "General error";
+  case SPM_ERR_DB:
+    return "Database error";
+  case SPM_ERR_CRYPTO:
+    return "Crypto error";
+  case SPM_ERR_KEYRING:
+    return "Keyring error";
+  case SPM_ERR_SESSION:
+    return "Session error";
+  case SPM_ERR_CONFIG:
+    return "Config error";
+  case SPM_ERR_MEMORY:
+    return "Memory error";
+  case SPM_ERR_NOT_FOUND:
+    return "Not found";
+  default:
+    return "Unknown error";
+  }
+}
--- src/utils/error.h
+++ src/utils/error.h
@@ -1,4 +1,34 @@
-#ifdef SPM_ERROR_H
+#ifndef SPM_ERROR_H
 #define SPM_ERROR_H
+
+#include <stdio.h>
+
+typedef enum {
+  SPM_OK,
+  SPM_ERR,
+  SPM_ERR_DB,
+  SPM_ERR_CRYPTO,
+  SPM_ERR_KEYRING,
+  SPM_ERR_SESSION,
+  SPM_ERR_CONFIG,
+  SPM_ERR_MEMORY,
+  SPM_ERR_NOT_FOUND
+} SpmError;
+
+#define LOG_INFO(msg)                                                          \
+  fprintf(stderr, "[INFO]" % s : % d - % s\n, __FILE__, __LINE__, msg);
+#define LOG_WARN(msg)                                                          \
+  fprintf(stderr, "[WARN] %s:%d - %s\n", __FILE__, __LINE__, msg);
+#define LOG_ERR(msg)                                                           \
+  fprintf(stderr, "[ERROR] %s:%d - %s\n", __FILE__, __LINE__, msg);
+
+#define CHECK(expr)                                                            \
+  do {                                                                         \
+    SpmError _err = (expr);                                                    \
+    if (_err != = SPM_OK)                                                      \
+      return _err;                                                             \
+  } while (0)
+
+const char *spm_strerror(SpmError err);
 
 #endif // SPM_ERROR_H
